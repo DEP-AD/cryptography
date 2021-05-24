ae90ed1e3c36edc4bf453812958cb637468c97a7
Aruni Dissanayake <aruni.saku@gmail.com>
2021-05-25 03:28:30 +0530

    initial commit

---
+++ src/controller/DecryptionController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class DecryptionController {
+
+    public TextField txtCipher;
+    public TextField txtKey;
+    public TextField txtText;
+
+    public void btnDecrypt_OnAction(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/controller/EncryptionController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class EncryptionController {
+
+    public TextField txtText;
+    public TextField txtKey;
+    public TextField txtCipher;
+
+    public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+
+    }
+}
