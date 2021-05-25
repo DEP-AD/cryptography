7a91c0bb4e72636db274de58437e0b9cbc063684
Aruni Dissanayake <aruni.saku@gmail.com>
2021-05-25 17:54:37 +0530

    encrypt method implement

--- src/controller/EncryptionController.java
+++ src/controller/EncryptionController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 
 public class EncryptionController {
@@ -10,6 +12,36 @@ public class EncryptionController {
     public TextField txtCipher;
 
     public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+        String text = txtText.getText();
+        String key = txtKey.getText();
 
+        if(text.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid text", ButtonType.OK).show();
+            txtText.requestFocus();
+            return;
+        }
+
+        if(key.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
+            txtKey.requestFocus();
+            return;
+        }
+
+        text += key;
+
+        String reversedText="";
+        for (int i = text.length()-1; i >= 0; i--) {
+            reversedText += text.charAt(i);
+        }
+        System.out.println(reversedText);
+
+        String cipherText="";
+        for (int i = 0; i < reversedText.length(); i++) {
+            int code  = reversedText.charAt(i);
+            code +=10;
+            char newChar=(char) code;
+            cipherText += newChar;
+        }
+        txtCipher.setText(cipherText);
     }
 }
